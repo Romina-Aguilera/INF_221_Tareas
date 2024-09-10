c8a0ea6d6a8f91b125f849a3b7dc4e11933077ce
Romina <romi.aguilera2003@gmail.com>
2024-09-10 09:55:35 -0300

    Implementación Total Sort

---
+++ Ordenamiento/Algoritmos/Sort.cpp
@@ -0,0 +1,36 @@
+#include "iostream"
+#include "algorithm"
+#include "vector"
+#include "string"
+#include "fstream"
+
+using namespace std;
+
+vector<int> pobladorVector(vector<int> elementos){
+    ifstream MyFile("Desordenado_1.txt");
+    int n;
+    while (MyFile >> n){
+        elementos.push_back(n);
+    }
+    MyFile.close();
+    return elementos;
+
+}
+
+void Imprimir_vector (vector<int> vect, int tam_vector){
+    for (int i = 0; i < tam_vector; i++){
+        cout << vect[i] << endl;
+    }
+}
+
+int main(){
+    vector<int> vect;
+    vect = pobladorVector(vect);
+
+    int tam_vect = vect.size();
+    sort(vect.begin(), vect.end());
+
+    Imprimir_vector(vect, tam_vect);
+
+    return 0;
+}
